758ca080c5f0bacbdf4c1852dba6a7521bf3dfd2
James Liam De Jesus <jamesliamdejesus@gmail.com>
2022-02-19 20:19:10 +0800

    added arduino and bluetooth connection but with error

--- mobile_application/FDMA/app/src/main/java/com/yorme/fdma/utilities/arduino/Ardcon.java
+++ mobile_application/FDMA/app/src/main/java/com/yorme/fdma/utilities/arduino/Ardcon.java
@@ -1,10 +1,15 @@
 package com.yorme.fdma.utilities.arduino;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.yorme.fdma.app.MainActivity;
@@ -13,19 +18,33 @@ import com.yorme.fdma.app.settings.ChangePassword;
 //Android to Arduino via bluetooth
 public class Ardcon extends AppCompatActivity {
     private static final int REQUEST_ENABLE_BT = 1;
+    public static final String ACTION_REQUEST_ENABLE = "android.bluetooth.adapter.action.REQUEST_ENABLE";
 
-    public void getBluetoothConnection(Context context){
+    public void getBluetoothConnection(Context context) {
         BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        try {
-            if (!bluetoothAdapter.isEnabled()) {
-                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-            }
-        } catch (NullPointerException nce){
+        if (bluetoothAdapter == null) {
             Toast.makeText(context, "This device does not support Bluetooth.", Toast.LENGTH_SHORT).show();
+        } else {
+            if(!bluetoothAdapter.isEnabled()){
+                Toast.makeText(context, "Bluetooth is not Enabled", Toast.LENGTH_SHORT).show();
+
+                Intent enableBtIntent = new Intent(ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+
+//                ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+//                        new ActivityResultContracts.StartActivityForResult(),
+//                        new ActivityResultCallback<ActivityResult>() {
+//                            @Override
+//                            public void onActivityResult(ActivityResult result) {
+//                                if (result.getResultCode() == Activity.RESULT_OK) {
+//                                    Intent data = result.getData();
+//                                }
+//                            }
+//                        });
+//
+//                someActivityResultLauncher.launch(enableBtIntent);
+            }
         }
 
     }
-
-    
 }
